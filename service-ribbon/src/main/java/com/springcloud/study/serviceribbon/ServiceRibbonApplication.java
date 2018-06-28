72e23d6660832a6e491ed4b77171c902c3ff152d
weizhong <weizhong@company.com>
2018-06-28 17:42:19 +0800

    练习同步

--- service-ribbon/src/main/java/com/springcloud/study/serviceribbon/ServiceRibbonApplication.java
+++ service-ribbon/src/main/java/com/springcloud/study/serviceribbon/ServiceRibbonApplication.java
@@ -4,6 +4,7 @@ import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServl
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
@@ -13,7 +14,7 @@ import org.springframework.web.client.RestTemplate;
 import javax.servlet.ServletRegistration;
 
 @SpringBootApplication
-@EnableHystrix
+@EnableCircuitBreaker
 @EnableHystrixDashboard
 public class ServiceRibbonApplication {
 
